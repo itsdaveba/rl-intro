77e533f87ee8d0e9f1c9c04dbf0034a9d2e422de
Dave Barragan <itsdaveba@gmail.com>
2024-05-10 21:01:24 +0100

    gridworld render

--- chapter03/gridworld.py
+++ chapter03/gridworld.py
@@ -12,16 +12,17 @@ class GridWorld(gym.Env):
         self.action_space = spaces.Discrete(4)
 
         base_rewards = [0, -1]
-        rd_keys = list(reward_dynamics.keys()) if reward_dynamics is not None else []
-        rd_from = [reward_dynamics[key]["from"] for key in rd_keys]
-        self.rewards = base_rewards + [reward_dynamics[key][1] for key in rd_keys]
+        self.rd_keys = list(reward_dynamics.keys()) if reward_dynamics is not None else []
+        self.rd_from = [reward_dynamics[key]["from"] for key in self.rd_keys]
+        self.rd_to = {reward_dynamics[key]["from"]: reward_dynamics[key]["to"] for key in self.rd_keys}
+        self.rewards = base_rewards + [reward_dynamics[key]["reward"] for key in self.rd_keys]
         self.prob = np.zeros(self.shape + (self.action_space.n,) + self.shape + (len(self.rewards),), dtype=np.float32)
 
         actions = np.array([[0, 1], [-1, 0], [0, -1], [1, 0]], dtype=np.int32)
         for state in product(*[range(i) for i in self.shape]):
             for action in range(self.action_space.n):
-                if state in rd_from:
-                    self.prob[state][action][reward_dynamics[state][0]][rd_keys.index(state) + len(base_rewards)] = 1.0
+                if state in self.rd_from:
+                    self.prob[state][action][self.rd_to[state]][self.rd_from.index(state) + len(base_rewards)] = 1.0
                 else:
                     new_state = state + actions[action]
                     if np.any(new_state // self.shape):
@@ -47,7 +48,20 @@ class GridWorld(gym.Env):
     def render(self):
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                print("x" if np.all(self.state == (i, j)) else ".", end=" ")
+                k = 0
+                if np.all(self.state == (i, j)):
+                    print("x", end=" ")
+                else:
+                    for rd_from in self.rd_from:
+                        if np.all(rd_from == (i, j)):
+                            print(self.rd_keys[self.rd_from.index(rd_from)], end=" ")
+                            break
+                        if np.all(self.rd_to[rd_from] == (i, j)):
+                            print(f"{self.rd_keys[self.rd_from.index(rd_from)]}'", end="")
+                            break
+                        k += 1
+                if k == len(self.rd_keys):
+                    print(".", end=" ")
             print()
 
 
